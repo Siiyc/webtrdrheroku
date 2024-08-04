c8a719c9321d767661d870c3caf82a0fd556f845
Siiyc <didenko203@gmail.com>
2024-08-04 15:50:14 +0300

    log JSON

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from flask import Flask, request, jsonify
-import os
 import logging
+import os
 app = Flask(__name__)
+
+# Настройка логирования
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
 @app.route('/webhook', methods=['POST'])
 def webhook():
     if request.method == 'POST':
         data = request.json
-        # Обработка полученных данныхgit
-        logger.info('Received webhook data: %s', data)
-        print(data)  # Вывод данных в консоль
+        if data:
+            logger.info('Received webhook data: %s', data)
+        else:
+            logger.warning('No JSON data received or JSON data is invalid.')
         return jsonify({'status': 'success'}), 200
 
 if __name__ == '__main__':

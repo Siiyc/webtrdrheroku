bb92a2f04923210bdfbc84664147d895ce451b76
Siiyc <didenko203@gmail.com>
2024-08-04 15:11:32 +0300

    smth new

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-
+import os
 app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
@@ -7,8 +7,8 @@ def webhook():
     if request.method == 'POST':
         data = request.json
         # Обработка полученных данных
-        print(data)
+        print(data)  # Вывод данных в консоль
         return jsonify({'status': 'success'}), 200
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))

db2720726efcb0bd506c116aac429c44f1bbba30
Siiyc <didenko203@gmail.com>
2024-08-09 00:17:58 +0300

    aaa

--- app.py
+++ app.py
@@ -1,23 +1,34 @@
 from flask import Flask, request, jsonify
-from telegram import Bot
+from telegram import Bot, InputFile
 import aiohttp
 import asyncio
 import json
+import os
+import tempfile
 
 app = Flask(__name__)
 
 # Ваш токен Telegram-бота и ID чата
 TELEGRAM_TOKEN = '7179465730:AAEFcAad5AG0HWGTlCJ0e3fv0G6ZL-cQ3AA'
-CHAT_ID = '-4269958834'
+CHAT_ID = '427720816'
 
 bot = Bot(token=TELEGRAM_TOKEN)
 
-async def send_message_async(message):
+async def send_message_async(message, json_file_path):
     async with aiohttp.ClientSession() as session:
+        # Отправка текстового сообщения
         url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
         data = {'chat_id': CHAT_ID, 'text': message, 'parse_mode': 'Markdown'}
         async with session.post(url, data=data) as response:
-            return await response.text()
+            await response.text()
+
+        # Отправка JSON-файла
+        url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendDocument"
+        with open(json_file_path, 'rb') as file:
+            files = {'document': file}
+            data = {'chat_id': CHAT_ID}
+            async with session.post(url, data=data, files=files) as response:
+                return await response.text()
 
 def format_message(data):
     # Извлечение данных из JSON
@@ -60,12 +71,20 @@ def webhook():
 
     # Форматируем сообщение
     message = format_message(data)
-    
-    # Запускаем асинхронную функцию в синхронном контексте
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    result = loop.run_until_complete(send_message_async(message))
-    loop.close()
+
+    # Сохраняем JSON во временный файл
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".json", mode='w') as temp_json_file:
+        json.dump(data, temp_json_file, indent=4)
+        temp_json_file_path = temp_json_file.name
+
+    try:
+        # Запускаем асинхронную функцию в синхронном контексте
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        result = loop.run_until_complete(send_message_async(message, temp_json_file_path))
+    finally:
+        # Удаляем временный файл
+        os.remove(temp_json_file_path)
 
     return jsonify({'status': 'success'}), 200
 

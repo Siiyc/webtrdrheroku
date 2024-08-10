c5ab7d485f8c7538f0344abd63faf5a410e491d9
Siiyc <didenko203@gmail.com>
2024-08-10 12:32:02 +0300

    213

--- app.py
+++ app.py
@@ -3,98 +3,50 @@ from telegram import Bot
 import aiohttp
 import asyncio
 import json
-import os
-import tempfile
 
 app = Flask(__name__)
 
 # Ваш токен Telegram-бота и ID чата
 TELEGRAM_TOKEN = '7179465730:AAEFcAad5AG0HWGTlCJ0e3fv0G6ZL-cQ3AA'
-CHAT_ID = '427720816'
+CHAT_ID = '-4269958834'
 
 bot = Bot(token=TELEGRAM_TOKEN)
 
-async def send_message_async(message, json_file_path = None):
+async def send_message_async(message):
     async with aiohttp.ClientSession() as session:
-        # Отправка текстового сообщения
         url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
         data = {'chat_id': CHAT_ID, 'text': message, 'parse_mode': 'Markdown'}
         async with session.post(url, data=data) as response:
-            await response.text()
-        if json_file_path == None:
-            return
-        # Отправка JSON-файла
-        url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendDocument"
-        form = aiohttp.FormData()
-        form.add_field('chat_id', CHAT_ID)
-        form.add_field('document', open(json_file_path, 'rb'), filename=os.path.basename(json_file_path), content_type='application/json')
-        
-        async with session.post(url, data=form) as response:
             return await response.text()
 
 def format_message(data):
     # Извлечение данных из JSON
-    alert_id = data.get('name', 'N/A')
-    side = data.get('side', 'N/A').capitalize()
-    continuation = data.get('continuation', 'N/A')
-    base = data.get('base', 'N/A')
+    content = data.get('name', 'N/A')
+    
     
     # Обработка списка markets
-    markets = data.get('markets', [])
-    if markets:
-        market = markets[0]  # Предполагаем, что интересует первый элемент
-        exchange = market.get('exchange', 'N/A')
-        symbol = market.get('symbol', 'N/A')
-        price = market.get('price', 'N/A')
-    else:
-        exchange = 'N/A'
-        symbol = 'N/A'
-        price = 'N/A'
-
+    
     # Создание форматированного сообщения
     formatted_message = (
-        f"**Alert ID:** {alert_id}\n"
-        f"**Side:** {side}\n"
-        f"**Continuation:** {continuation} minutes\n\n"
-        f"**Market Information:**\n"
-        f"   *Base:* {base}\n"
-        f"   *Symbol:* {symbol}\n"
-        f"   *Exchange:* {exchange}\n"
-        f"   *Price:* {price}\n\n"
-        f"{data.get('message', 'No additional message')}"
+        f"**Info:** {content}\n"
+        
     )
     return formatted_message
 
 @app.route('/', methods=['POST'])
 def webhook():
     data = request.json
-    if data == None:
-        message = 'nihuya'
-        try:
-            # Запускаем асинхронную функцию в синхронном контексте
-            loop = asyncio.new_event_loop()
-            asyncio.set_event_loop(loop)
-            result = loop.run_until_complete(send_message_async(message))
-        finally:
-            # Удаляем временный файл
-            print()
+    if not data:
+        return jsonify({'error': 'No JSON data received'}), 400
 
     # Форматируем сообщение
-    #message = format_message(data)
-    message = 'huy'
-    # Сохраняем JSON во временный файл
-    with tempfile.NamedTemporaryFile(delete=False, suffix=".json", mode='w') as temp_json_file:
-        json.dump(data, temp_json_file, indent=4)
-        temp_json_file_path = temp_json_file.name
-
-    try:
-        # Запускаем асинхронную функцию в синхронном контексте
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-        result = loop.run_until_complete(send_message_async(message))
-    finally:
-        # Удаляем временный файл
-        os.remove(temp_json_file_path)
+    message = format_message(data)
+    
+    # Запускаем асинхронную функцию в синхронном контексте
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    result = loop.run_until_complete(send_message_async(message))
+    loop.close()
 
     return jsonify({'status': 'success'}), 200
 
